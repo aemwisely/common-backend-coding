2f5c424106f8196395b43246ea217b69a8d454ee
aemjirapat.tr <aemjirapat.tr@gmail.com>
2024-10-09 12:19:26 +0700

    Add storage module and media object entity

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,4 @@ export * from './config';
 export * from './shared';
 export * from './groups';
 export * from './base';
+export * from './storage';
--- libs/core/src/entities/media-object.entity.ts
+++ libs/core/src/entities/media-object.entity.ts
@@ -1,5 +1,38 @@
 import { CommonEntity } from 'libs/common';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'media_object' })
-export class MediaObjectEntity extends CommonEntity {}
+export class MediaObjectEntity extends CommonEntity {
+  @Column()
+  filename: string;
+
+  @Column()
+  originalFilename: string;
+
+  @Column()
+  alternativeText: string;
+
+  @Column()
+  width: string;
+
+  @Column()
+  height: string;
+
+  @Column()
+  mimeType: string;
+
+  @Column()
+  key: string;
+
+  @Column()
+  size: number;
+
+  @Column()
+  dueDate: Date;
+
+  @Column()
+  path: string;
+
+  @Column()
+  url: string;
+}
